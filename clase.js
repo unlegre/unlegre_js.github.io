84682468cc84f884265243a7b5b4483db3b04d3c
unlegre <65373797+unlegre@users.noreply.github.com>
2022-06-27 10:46:46 -0300

    Add files via upload

---
+++ clase.js
@@ -0,0 +1,18 @@
+
+
+for (let i = 1; i <= 5; i++) {
+    // En cada repetición solicitamos un nombre.
+    let ingresarNombre = prompt("Ingresar nombre para dar turno");
+    // Informamos el turno asignado usando el número de repetición (i).
+    alert("ingreso "+ingresarNombre+" "+"su N° Turno  es: "+i);
+}
+
+
+for (let i = 1; i <= 5; i++) {
+    // Solicitamosun valor al usuario
+let ingresarNumero = parseInt(prompt("Ingresar 5 Numeros y se sumara 5 a cada uno de ellos"))
+    let resultado = ingresarNumero + 5;
+    alert( "usted ingreso :"+ ingresarNumero +" + 5 = "+ resultado);
+}
+
+
